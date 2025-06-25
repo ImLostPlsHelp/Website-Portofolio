31e0fa257ed2d7fef32fce8ac10315ef593ac08f
ImLostPlsHelp <maurits1005@gmail.com>
2025-06-25 19:54:56 +0700

    Hero section ongoing

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@
 // import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/navbar";
+import Hero from "./components/hero";
 
 function App() {
   return (
     <>
       <div>
         <Navbar />
+      </div>
+      <div>
+        <Hero />
       </div>
     </>
   );
---
+++ src/components/hero.tsx
@@ -0,0 +1,13 @@
+function Hero() {
+  return (
+    <div className="hero d-flex flex-column justify-content-center align-items-center text-center text-white bg-dark vh-100">
+      <h1 className="display-4">Hello, I'm Maurits Sitinjak</h1>
+      <p className="lead">A passionate software developer</p>
+      <a href="#portfolio" className="btn btn-primary btn-lg">
+        View Portfolio
+      </a>
+    </div>
+  );
+}
+
+export default Hero;
