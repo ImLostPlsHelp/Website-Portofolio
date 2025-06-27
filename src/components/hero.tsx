6eceb33513c207134c3b7d70292f364f2f86519a
ImLostPlsHelp <maurits1005@gmail.com>
2025-06-27 14:57:55 +0700

    Last commit was bad, this one fixed it

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -44,13 +44,13 @@ function Hero() {
             </div>
           </aside>
         </div>
-        <div className="tech-stack container-fluid d-flex flex-column align-items-center justify-content-start mt-5 border border-primary rounded-2">
-          <h3 className="text-white display-4">Tech Stack</h3>
-          <div className="row">
-            <div className="col">
-              <p className="text-white">Javascript</p>
+        <div className="tech-stack container-fluid d-flex flex-column align-items-center justify-content-center mt-5 border border-primary rounded-2">
+          <h3 className="text-white display-3">Tech Stack</h3>
+          <div className="row gx-4 gy-4 w-100 d-flex justify-content-center align-items-center text-center mt-4">
+            <div className="col-6 col-md-4 col-lg-3">
+              <p className="text-white heading-3">Javascript</p>
             </div>
-            <div className="col">
+            <div className="col-6 col-md-4 col-lg-3">
               <img
                 src="/bootstrap-fill.svg"
                 alt=""
@@ -58,10 +58,10 @@ function Hero() {
                 height="20"
                 style={{ filter: "brightness(0) invert(1)" }}
               />
-              <p className="text-white">Bootstrap</p>
+              <p className="text-white heading-3">Bootstrap</p>
             </div>
-            <div className="col">
-              <p className="text-white">React</p>
+            <div className="col-6 col-md-4 col-lg-3">
+              <p className="text-white heading-3">React</p>
             </div>
           </div>
         </div>
